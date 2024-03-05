4ad8865bfdde63731a4860a1d7cf70699efd4d4d
Geoffrey O'Connor <gaoconnor@partners.org>
2024-03-05 08:27:48 -0500

    add local time to overview string

--- WeatherApp.Infrastructure/Builders/ClothesBuilderBase.cs
+++ WeatherApp.Infrastructure/Builders/ClothesBuilderBase.cs
@@ -52,6 +52,6 @@ public abstract class ClothesBuilderBase : IClothesBuilder
     public abstract void BuildHat();
     public abstract void BuildTopLayers();
     public abstract void BuildBottomLayer();
-    public void BuildOverview() => _clothes.Overview = $"{DateTime.Now}: {_layerCustomizations.Weather.City} Feels like: {_layerCustomizations.Weather.FeelsLikeTemp}, Actual Temp: {_layerCustomizations.Weather.Temperature}";
+    public void BuildOverview() => _clothes.Overview = $"{DateTime.Now.ToLocalTime()}: {_layerCustomizations.Weather.City} Feels like: {_layerCustomizations.Weather.FeelsLikeTemp}, Actual Temp: {_layerCustomizations.Weather.Temperature}";
     public Clothes GetClothes() => _clothes;
 }
