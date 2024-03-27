fe4104873bd0827804e115dd4003a47189fa5649
Geoffrey O'Connor <gaoconnor@mgb.org>
2024-03-27 10:08:37 -0400

    create mocks folder

--- WeatherApp.UI/src/App.test.tsx
+++ WeatherApp.UI/src/App.test.tsx
@@ -2,47 +2,62 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { App } from './App';
+import { mockNavigatorGeolocation } from './test/mocks/mockNavigatorGeolocation';
 
-const mockNavigatorGeolocation = () => {
-  const clearWatchMock = jest.fn();
-  const getCurrentPositionMock = jest.fn();
-  const watchPositionMock = jest.fn();
-
-  const geolocation = {
-    clearWatch: clearWatchMock,
-    getCurrentPosition: getCurrentPositionMock,
-    watchPosition: watchPositionMock,
-  };
-
-  Object.defineProperty(global.navigator, 'geolocation', {
-    value: geolocation,
-  });
-
-  return { clearWatchMock, getCurrentPositionMock, watchPositionMock };
-};
-
-test('renders clothes heading and HOme Page error failed to load clothes', async () => {
-  const { getCurrentPositionMock } = mockNavigatorGeolocation();
-  getCurrentPositionMock.mockImplementation((success) => {
-    success({
-      coords: {
-        latitude: 51.1,
-        longitude: 45.3
-      }
-    })
-  });
-
+const renderComponent = async () => {
   render(
     <MemoryRouter>
       <App />
     </MemoryRouter>
   );
+  await screen.findAllByRole('link');
+}
 
-  const header = screen.getByRole('heading', { name: /clothes for current weather/i })
+describe('<App />', () => {
 
-  expect(header).toBeInTheDocument();
+  /*test('renders navigation elements', async () => {
+    await renderComponent();
 
-  const error = await screen.findByRole('heading', { name: /Error: failed to load clothes/i })
-  expect(error).toBeInTheDocument();
+    const homeLink = await screen.findByRole('link', { name: /home/i });
+    const loginLink = await screen.findByRole('link', { name: /log in/i });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(loginLink).toBeInTheDocument();
+  })*/
+
+  test('renders clothes heading and HOme Page error failed to load clothes', async () => {
+
+    //const { getCurrentPositionMock, clearWatchMock } = mockNavigatorGeolocation();
+    const { getCurrentPositionMock } = mockNavigatorGeolocation();
+
+    getCurrentPositionMock.mockImplementation((success) => {
+      success({
+        coords: {
+          latitude: 51.1,
+          longitude: 45.3
+        }
+      })
+    });
+
+    await renderComponent();
+
+    const header = screen.getByRole('heading', { name: /clothes for current weather/i })
+
+    expect(header).toBeInTheDocument();
+
+    const error = await screen.findByRole('heading', { name: /error: failed to load clothes/i })
+    expect(error).toBeInTheDocument();
+
+    //clearWatchMock();
+
+  });
+
+  test('You need to allow location for the app to work', async () => {
+
+    await renderComponent();
+
+    const error = await screen.findByRole('heading', { name: /error: you need to allow location for the app to work/i });
+    expect(error).toBeInTheDocument();
+  })
 
 });
---
+++ WeatherApp.UI/src/test/mocks/mockNavigatorGeolocation.ts
@@ -0,0 +1,17 @@
+export const mockNavigatorGeolocation = () => {
+  const clearWatchMock = jest.fn();
+  const getCurrentPositionMock = jest.fn();
+  const watchPositionMock = jest.fn();
+
+  const geolocation = {
+    clearWatch: clearWatchMock,
+    getCurrentPosition: getCurrentPositionMock,
+    watchPosition: watchPositionMock,
+  };
+
+  Object.defineProperty(global.navigator, 'geolocation', {
+    value: geolocation,
+  });
+
+  return { clearWatchMock, getCurrentPositionMock, watchPositionMock };
+};
