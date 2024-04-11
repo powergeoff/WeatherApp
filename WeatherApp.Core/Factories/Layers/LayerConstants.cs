d54d1b0928ae74b5b9efe0ff0fa11302f297a502
Geoffrey O'Connor <gaoconnor@mgb.org>
2024-04-11 05:21:15 -0400

    change jacketTemp from 60 to 50

--- WeatherApp.Core/Factories/Layers/LayerConstants.cs
+++ WeatherApp.Core/Factories/Layers/LayerConstants.cs
@@ -7,7 +7,7 @@ public class LayerConstants
     public const int TShirtMaxTemp = 95;
     public const int LongSleeveTShirtMaxTemp = 80;
     public const int SweatShirtMaxTemp = 70;
-    public const int JacketMaxTemp = 60;
+    public const int JacketMaxTemp = 50;
     public const int HeavyDutytMaxTemp = 20;
     public const int WinterHatMaxTemp = 50;
 
