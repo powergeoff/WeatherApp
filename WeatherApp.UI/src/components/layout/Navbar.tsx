faae2712ba602e07fe62dc33b7054c383681708e
Geoffrey O'Connor <gaoconnor@mgb.org>
2024-05-01 14:33:58 -0400

    repsonsive menu and new logo

--- WeatherApp.UI/src/components/layout/Navbar.tsx
+++ WeatherApp.UI/src/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { FaGithub } from 'react-icons/fa';
+import { WiDayStormShowers } from "react-icons/wi";
 import { Link } from 'react-router-dom';
 import { AuthInfoContext, IAuthInfoContext } from '../../context/authInfo/AuthInfoContext';
 
@@ -13,14 +13,37 @@ const Navbar: React.FC<Props> = ({ title }: Props) => {
     return (
         <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
             <div className='container mx-auto'>
+                {/* start hamburger menu */}
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <Link to='/'>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/login'>
+                                Login
+                            </Link>
+                        </li>
+                        {user?.userName !== '' ? <li><Link to='/logout' onClick={logout} >
+                            Logout
+                        </Link></li> : ''}
+                    </ul>
+                </div>
+                {/* end hamburger menu */}
                 <div className='flex-none px-2 mx-2'>
-                    <FaGithub className='inline pr-2 text-3xl' />
+
                     <Link to='/' className='text-lg font-bold align-middle'>
-                        {title}
+                        <WiDayStormShowers className='inline pr-2 text-6xl' />
+                        <span className='hidden lg:inline '>{title}</span>
                     </Link>
                 </div>
 
-                <div className='flex-1 px-2 mx-2'>
+                <div className='hidden lg:block flex-1 px-2 mx-2'>
                     <div className='flex justify-end'>
                         <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
                             Home
